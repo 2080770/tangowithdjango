0791d88be651b7fb0d9b39036032a527bb31adb8
Hristo Radev <hristoradev@mail.bg>
2015-01-25 23:02:17 +0000

    Chapter 10 - completed

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -47,7 +47,7 @@ class PageForm(forms.ModelForm):
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-
+    email = forms.CharField()
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
